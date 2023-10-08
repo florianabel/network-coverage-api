5dde4270c19264ce044990f6e106a9813175f868
florianabel <mail@florianabel.com>
2023-10-08 23:27:36 +0200

    Basic refactoring

--- src/main.py
+++ src/main.py
@@ -5,10 +5,8 @@ from pydantic import ValidationError
 
 from .models import NetworkCoverage, Feature, ProviderCoverage
 from .utils import (
-    closest_point_orange,
-    closest_point_sfr,
-    closest_point_free,
-    closest_point_bouygue,
+    api_call_gouv,
+    gps_to_coverage,
 )
 
 app = FastAPI()
@@ -16,46 +14,12 @@ app = FastAPI()
 
 @app.get("/", response_model_by_alias=True)
 async def network_coverage(q: str) -> Union[NetworkCoverage, bool]:
-    response = requests.get(f"https://api-adresse.data.gouv.fr/search/?q={q}")
-    res_json = response.json()
-    features = res_json.get('features', None)
+    features = api_call_gouv(q)
     if features and len(features) > 0 :
         try:
             feature_obj = Feature.model_validate(features[0])
         except ValidationError as e:
             print(e)
             raise
-        gps_coordinates = feature_obj.geometry.coordinates
-        df_closest_point_orange = closest_point_orange(tuple(gps_coordinates))
-        df_closest_point_sfr = closest_point_sfr(tuple(gps_coordinates))
-        df_closest_point_free = closest_point_free(tuple(gps_coordinates))
-        df_closest_point_bouygue = closest_point_bouygue(tuple(gps_coordinates))
-       
-        provider_data_orange = ProviderCoverage(
-            two_g=df_closest_point_orange['2G'] == 1,
-            three_g=df_closest_point_orange['3G'] == 1,
-            four_g=df_closest_point_orange['4G'] == 1,
-        )
-        provider_data_sfr = ProviderCoverage(
-            two_g=df_closest_point_sfr['2G'] == 1,
-            three_g=df_closest_point_sfr['3G'] == 1,
-            four_g=df_closest_point_sfr['4G'] == 1,
-        )
-        provider_data_free = ProviderCoverage(
-            two_g=df_closest_point_free['2G'] == 1,
-            three_g=df_closest_point_free['3G'] == 1,
-            four_g=df_closest_point_free['4G'] == 1,
-        )
-        provider_data_bouygue = ProviderCoverage(
-            two_g=df_closest_point_bouygue['2G'] == 1,
-            three_g=df_closest_point_bouygue['3G'] == 1,
-            four_g=df_closest_point_bouygue['4G'] == 1,
-        )
-        coverage = NetworkCoverage(
-            orange=provider_data_orange,
-            sfr=provider_data_sfr,
-            free=provider_data_free,
-            bouygue=provider_data_bouygue,
-        )
-        return coverage
+        return gps_to_coverage(feature_obj.geometry.coordinates)
     return HTTPException(status_code=204)
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,22 @@
 import pandas as pd
 from geopy import distance
+import requests
+
+from typing import Optional
+
+from .models import NetworkCoverage, ProviderCoverage
+
+PROVIDER_CODES = {
+    "orange": 20801,
+    "sfr": 20810,
+    "free": 20815,
+    "bouygue": 20820,
+}
 
 coverage_data = pd.read_csv('src/data/network_coverage_gps.csv', delimiter=';')
-coverage_data_orange = coverage_data[coverage_data['Operateur'] == 20801]
-coverage_data_sfr = coverage_data[coverage_data['Operateur'] == 20810]
-coverage_data_free = coverage_data[coverage_data['Operateur'] == 20815]
-coverage_data_bouygue = coverage_data[coverage_data['Operateur'] == 20820]
 
-def _closest_point(df: pd.DataFrame, point: tuple) -> pd.DataFrame:
+
+def closest_point_provider(df: pd.DataFrame, point: tuple) -> pd.DataFrame:
     df['distance'] = df.apply(
         lambda x: distance.great_circle(
             (x['lat'], x['long']), 
@@ -17,14 +26,24 @@ def _closest_point(df: pd.DataFrame, point: tuple) -> pd.DataFrame:
     )
     return df[df.distance == df.distance.min()]
 
-def closest_point_orange(point: tuple) -> pd.DataFrame:
-    return _closest_point(coverage_data_orange, point)
 
-def closest_point_sfr(point: tuple) -> pd.DataFrame:
-    return _closest_point(coverage_data_sfr, point)
+def api_call_gouv(q: str) -> Optional[dict]:
+    api_url = f"https://api-adresse.data.gouv.fr/search/?q={q}"
+    response = requests.get(api_url)
+    res_json = response.json()
+    features = res_json.get('features', None)
+    return features
 
-def closest_point_free(point: tuple) -> pd.DataFrame:
-    return _closest_point(coverage_data_free, point)
 
-def closest_point_bouygue(point: tuple) -> pd.DataFrame:
-    return _closest_point(coverage_data_bouygue, point)
+def gps_to_coverage(gps_coordinates: tuple) -> NetworkCoverage:
+    coverage_collection: dict[str, ProviderCoverage] = {}
+    for provider, provider_code in PROVIDER_CODES.items():
+        df = coverage_data[coverage_data['Operateur'] == provider_code]
+        df_closest_point = closest_point_provider(df, gps_coordinates)
+        provider_coverage = ProviderCoverage(
+            two_g=df_closest_point['2G'] == 1,
+            three_g=df_closest_point['3G'] == 1,
+            four_g=df_closest_point['4G'] == 1,
+        )
+        coverage_collection[provider] = provider_coverage
+    return NetworkCoverage.model_validate(coverage_collection)
