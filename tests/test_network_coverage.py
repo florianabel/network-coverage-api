3ba1282d85328c3d7d9c2fb31a3984c8f8a9707e
florianabel <mail@florianabel.com>
2023-10-08 22:45:34 +0200

    updated test naming

--- tests/test_network_coverage.py
+++ tests/test_network_coverage.py
@@ -5,7 +5,7 @@ from src.main import app
 client = TestClient(app)
 
 
-def test_root():
+def test_network_coverage():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {
