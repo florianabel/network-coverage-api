493bce3a7c5858972728d338f72701036a529718
florianabel <mail@florianabel.com>
2023-10-08 22:56:28 +0200

    fix: updated test

--- tests/test_network_coverage.py
+++ tests/test_network_coverage.py
@@ -6,11 +6,27 @@ client = TestClient(app)
 
 
 def test_network_coverage():
-    response = client.get("/")
+    response = client.get("/?q=42+rue+papernest+75011+Paris")
     assert response.status_code == 200
     assert response.json() == {
-        "orange": {"2G": True, "3G": False, "4G": True},
-        "sfr": {"2G": True, "3G": False, "4G": True},
-        "free": {"2G": True, "3G": False, "4G": True},
-        "bouygue": {"2G": True, "3G": False, "4G": True},
+        "orange": {
+            "2G": True,
+            "3G": True,
+            "4G": False,
+        },
+        "sfr": {
+            "2G": True,
+            "3G": False,
+            "4G": False,
+        },
+        "free": {
+            "2G": False,
+            "3G": True,
+            "4G": True,
+        },
+        "bouygue": {
+            "2G": True,
+            "3G": False,
+            "4G": False,
+        },
     }
